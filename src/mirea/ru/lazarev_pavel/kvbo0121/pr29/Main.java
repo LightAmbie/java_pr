98a619a929f929a643a595cf7ce7944a9bf7ca27
LightAmbie <lightambie@gmail.com>
2022-12-06 09:35:54 +0300

    Initial commit

---
+++ src/mirea/ru/lazarev_pavel/kvbo0121/pr29/Main.java
@@ -0,0 +1,22 @@
+package mirea.ru.lazarev_pavel.kvbo0121.pr29;
+
+import java.util.*;
+
+
+public class Main
+{
+    public static void main(String[] args)
+
+    {
+        Scanner con = new Scanner(System.in);
+        int n = con.nextInt();
+        int res = 0;
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                res += con.nextInt();
+        res /= 2;
+        System.out.println(res);
+    }
+
+}
