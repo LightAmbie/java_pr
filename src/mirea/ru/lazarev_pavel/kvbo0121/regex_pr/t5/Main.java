ed00c224abd6643da4eb96d16feff69741071e9b
LightAmbie <lightambie@gmail.com>
2022-12-06 09:35:03 +0300

    Initial commit

--- src/mirea/ru/lazarev_pavel/kvbo0121/regex_pr/t5/Main.java
+++ src/mirea/ru/lazarev_pavel/kvbo0121/regex_pr/t5/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while (true) {
-            System.out.println(isPrice(in.nextLine()));
+            System.out.println(isSecurePassword(in.nextLine()));
         }
     }
 
     final static Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])([\\w]){8,}$");
 
-    public static boolean isPrice(String string) {
+    public static boolean isSecurePassword(String string) {
         Matcher matcher = pattern.matcher(string);
         return matcher.matches();
     }
